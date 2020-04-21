24a48a8f24637eed60383224a29c33dfc177d6cf
John Ouellet <john@thinktandem.io>
2020-04-21 16:05:30 -0400

    Cleanup code

--- src/Command/GenerateXmlCommand.php
+++ src/Command/GenerateXmlCommand.php
@@ -5,11 +5,8 @@ namespace App\Command;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
-use Symfony\Component\Console\Question\Question;
-use Symfony\Component\Console\Question\ConfirmationQuestion;
 
 class GenerateXmlCommand extends Command {
 
@@ -51,7 +48,6 @@ class GenerateXmlCommand extends Command {
    * @inheritdoc
    */
   protected function execute(InputInterface $input, OutputInterface $output) {
-
     $io = new SymfonyStyle($input, $output);
     if ($this->fileName = $input->getArgument('filename')) {
       // Check if XML.
@@ -65,6 +61,7 @@ class GenerateXmlCommand extends Command {
       $this->createDirectoryStructureAndFile();
       $this->processRows();
 
+      // Generate the file.
       if (!empty($this->finalContent)) {
         $this->generateContent();
         $io->success("File generated.");
